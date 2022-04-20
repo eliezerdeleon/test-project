ce2d64b8e0434e7c21112cab1279bd692f299073
eliezerdeleon <djnb.nickyblast@gmail.com>
2022-04-20 08:40:20 -0400

    master

--- app/src/test/java/eliezerTest/runner/Runner.java
+++ app/src/test/java/eliezerTest/runner/Runner.java
@@ -1,19 +1,19 @@
 package eliezerTest.runner;
 
 import org.junit.runner.RunWith;
-import cucumber.api.CucumberOptions;
 
-@RunWith(cucumber.class)
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
 @CucumberOptions(
     features = "src/test/resources/Features",
     glue = "steps"
 
 )
 
-public class runner{
+public class Runner {
     
 }
 
 
-
-)    
